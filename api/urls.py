f33aafdfde0667d2666ddb96f6faac1e70256106
Rohit Thakre <thake785@gmail.com>
2023-11-16 10:47:18 +0530

    urls configured.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
+from api import views
 
+router = routers.DefaultRouter()
+router.register(r'company', views.CompanyViewSet)
 
-# urlpatterns = [
-#     path()
-# ]
+urlpatterns = [
+    path('',include(router.urls),)
+]
