42cb5714c8105dfd67e0dfdd9e5228d60539d841
Rohit Thakre <thake785@gmail.com>
2023-11-16 10:29:38 +0530

    model created.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Company(models.Model):
+    
+    name = models.CharField(max_length=50)
+    CEO = models.CharField(max_length=100)
+    type = models.CharField(choices=(('IT', 'IT'), 
+                                     ('Non IT', 'Non IT')
+                                     ))
+    about = models.TextField()
+    base = models.CharField(max_length=50)
+    active = models.BooleanField(default=1)
+
+    created = models.DateTimeField(auto_now_add= True)
+    by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return 
